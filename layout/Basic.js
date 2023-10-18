e9ebddaa68624df6bd76d45b1f65d16ec718ded2
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-18 15:55:29 +0200

    Update Basic.js

--- layout/Basic.js
+++ layout/Basic.js
@@ -8,7 +8,7 @@ export default function Basic(props) {
         <meta name="description" content={props.metaDescription} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="w-full px-5 lg:flex lg:justify-between  max-w-[1250px] mx-auto mt-20 min-h-[700px] rounded-lg relative bg-blue-500">
+      <main className="w-full px-5 lg:flex lg:justify-between  max-w-[1250px] mx-auto relative">
         {props.children}
       </main>
       <footer></footer>
