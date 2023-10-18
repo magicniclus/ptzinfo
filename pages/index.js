d9dd3e1c6dafdb83353f51c805beea937be68286
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-18 16:20:33 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 import Basic from "../layout/Basic";
 
 export default function Home() {
-  return (
-    <Basic>
-      <h1 className="text-red-500">Hello word</h1>
-    </Basic>
-  );
+  return <Basic></Basic>;
 }
