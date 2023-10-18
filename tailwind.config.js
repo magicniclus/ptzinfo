9e835e61a9c2b692d5bfdc6f2177c72cc579ca29
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-18 15:52:05 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        yan: ["Yanone Kaffeesatz", "sans-serif"],
+        manrope: ["Manrope", "sans-serif"],
+        mont: ["Montserrat", "sans-serif"],
+        sans: [
+          "Roboto",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        grayPrimary: "#FCFCFC",
+      },
+    },
+  },
+  plugins: [],
+};
