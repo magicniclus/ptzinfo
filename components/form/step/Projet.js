efbbef9439e469276623b6fc4a6f3c87bd263d50
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-19 18:27:26 +0200

    Create Projet.js

---
+++ components/form/step/Projet.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import Select from "../selected/Select";
+
+const Projet = () => {
+  const options = [
+    "Je recherche un bien",
+    "J'ai deja acquis un bien",
+    "Je ne sais pas",
+  ];
+
+  const dispatch = useDispatch();
+  const [selectedType, setSelectedType] = useState(null);
+
+  useEffect(() => {
+    dispatch({
+      type: "SET_CLIENT_INFORMATION",
+      payload: { projet: selectedType },
+    });
+  }, [selectedType]);
+
+  return (
+    <>
+      <h2
+        className="text-2xl font-light lg:my-0 my-5"
+        style={{ color: "#3b82f6" }}
+      >
+        Ou en êtes vous dans votre projet ?
+      </h2>
+      <div className="flex flex-col">
+        <Select options={options} onChange={setSelectedType} />
+      </div>
+    </>
+  );
+};
+
+export default Projet;
