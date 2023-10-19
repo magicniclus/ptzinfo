245f3d0711da456754e1c086563a370c79f1e54a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-19 11:39:45 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+import BreadCrumb from "../components/BreadCrumb";
+import CTABanner from "../components/CTABanner";
+import EvaluationBanner from "../components/EvaluationBanner";
+import Faq from "../components/Faq";
 import Hero from "../components/Hero";
 import Basic from "../layout/Basic";
 
@@ -5,6 +9,10 @@ export default function Home() {
   return (
     <Basic>
       <Hero />
+      <BreadCrumb list={["ACCUEIL", "ESTIMATION"]} />
+      <EvaluationBanner />
+      <Faq />
+      <CTABanner />
     </Basic>
   );
 }
