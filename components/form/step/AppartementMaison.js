a417a377628c1aed2e0401fa39bb991d3496fb62
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-19 14:49:03 +0200

    Create AppartementMaison.js

---
+++ components/form/step/AppartementMaison.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import CardWithImg from "../cards/CardWithImg";
+import { useSelector, useDispatch } from "react-redux";
+const AppartementMaison = () => {
+  const dispatch = useDispatch();
+  const [selectedType, setSelectedType] = useState(null);
+
+  useEffect(() => {
+    dispatch({
+      type: "SET_CLIENT_INFORMATION",
+      payload: { type: selectedType },
+    });
+  }, [selectedType]);
+  return (
+    <>
+      <h2
+        className="text-2xl font-light lg:my-0 my-5"
+        style={{ color: "#3b82f6" }}
+      >
+        Type de bien :
+      </h2>
+      <div className="flex flex-col">
+        {["Maison", "Appartement"].map((type, idx) => (
+          <div className={idx === 1 && "mt-3"}>
+            <CardWithImg
+              key={type}
+              type={type}
+              onSelect={setSelectedType}
+              typeSelected={selectedType}
+              margin="35px"
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default AppartementMaison;
