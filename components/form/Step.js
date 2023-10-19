b4d58b205bcc983dd738045b2e4968d5c51d0158
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-19 18:27:17 +0200

    Update Step.js

--- components/form/Step.js
+++ components/form/Step.js
@@ -2,32 +2,28 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import AppartementMaison from "./step/AppartementMaison";
 import { useRouter } from "next/router";
+import SituationPersonnelle from "./step/SituationPersonnelle";
+import SituationProfessionnelle from "./step/SituationProfessionnelle";
+import RevenusFiscal from "./step/RevenusFiscal";
+import NbrPartFoyer from "./step/NbrPartFoyer";
+import Projet from "./step/Projet";
 
 const Step = () => {
   const [error, setError] = useState(null);
   const step = useSelector((state) => state?.stepInProgress);
 
   const type = useSelector((state) => state?.clientInfomation?.type);
-  const surface = useSelector((state) => state?.clientInfomation?.surface);
-  const nbrPieces = useSelector((state) => state?.clientInfomation?.pieces);
-  const nbrChambres = useSelector((state) => state?.clientInfomation?.chambres);
-  const annee = useSelector((state) => state?.clientInfomation?.annee);
-  const etages = useSelector((state) => state?.clientInfomation?.etages);
-  const niveaux = useSelector((state) => state?.clientInfomation?.niveaux);
-  const espacesExterieurs = useSelector(
-    (state) => state?.clientInfomation?.espacesExterieurs
+  const situationPersonnelle = useSelector(
+    (state) => state?.clientInfomation?.situationPersonnelle
   );
-  const standing = useSelector((state) => state?.clientInfomation?.standing);
-  const vue = useSelector((state) => state?.clientInfomation?.vue);
-  const oriantation = useSelector(
-    (state) => state?.clientInfomation?.oriantation
+  const situationProfessionnelle = useSelector(
+    (state) => state?.clientInfomation?.situationProfessionnelle
   );
-  const travaux = useSelector((state) => state?.clientInfomation?.travaux);
-  const atouts = useSelector((state) => state?.clientInfomation?.atouts);
-  const dpe = useSelector((state) => state?.clientInfomation?.dpe);
-  const ges = useSelector((state) => state?.clientInfomation?.ges);
-  const contrat = useSelector((state) => state?.clientInfomation?.contrat);
-  const vente = useSelector((state) => state?.clientInfomation?.vente);
+  const revenusFiscal = useSelector(
+    (state) => state?.clientInfomation?.revenusFiscal
+  );
+  const nbrDePart = useSelector((state) => state?.clientInfomation?.nbrDePart);
+  const projet = useSelector((state) => state?.clientInfomation?.projet);
 
   const dispatch = useDispatch();
 
@@ -45,8 +41,23 @@ const Step = () => {
       case 2:
         return <AppartementMaison />;
 
+      case 3:
+        return <SituationPersonnelle />;
+
+      case 4:
+        return <SituationProfessionnelle />;
+
+      case 5:
+        return <RevenusFiscal />;
+
+      case 6:
+        return <NbrPartFoyer />;
+
+      case 7:
+        return <Projet />;
+
       default:
-        return <AppartementMaison />;
+        return;
     }
   };
   const handleStep = (e) => {
@@ -54,60 +65,28 @@ const Step = () => {
     dispatch({ type: "UPDATE_SIMULATEUR_STEP" });
   };
 
-  useEffect(() => {
-    if (step === 5 && nbrChambres > nbrPieces) {
-      setError(
-        "Le nombre de chambres ne peut pas être supérieur au nombre de pièces"
-      );
-    } else setError(null);
-  }, [nbrChambres]);
-
   const isButtonDisabled = () => {
     switch (step) {
       case 2:
         return !type;
 
       case 3:
-        return !surface;
+        return !situationPersonnelle;
 
       case 4:
-        return !nbrPieces;
+        return !situationProfessionnelle;
 
       case 5:
-        return !nbrChambres || nbrChambres > nbrPieces;
+        return !revenusFiscal;
 
       case 6:
-        return !annee;
+        return !nbrDePart;
 
       case 7:
-        if (type === "Appartement") {
-          return !etages || !niveaux;
-        }
-        return !etages;
-
-      case 8:
-        return false;
-
-      case 9:
-        return !standing;
-
-      case 10:
-        return !vue || oriantation.length === 0;
-
-      case 11:
-        return travaux === null || travaux === undefined;
-
-      case 12:
-        return false;
-
-      case 13:
-        return !dpe || !ges;
-
-      case 14:
-        return !contrat || !vente;
+        return !projet;
 
       default:
-        return false;
+        return true;
     }
   };
 
@@ -116,7 +95,7 @@ const Step = () => {
   };
 
   return (
-    <form className="w-full min-h-[300px] h-full flex flex-col justify-between bg-opacity-50 rounded-md mt-10">
+    <form className="w-full min-h-[250px] h-full flex flex-col justify-between bg-opacity-50 rounded-md mt-10">
       {handleComponent()}
       <div className="flex items-center lg:mb-0 mb-5">
         <button
@@ -127,9 +106,9 @@ const Step = () => {
             backgroundColor: "#3b82f6",
             opacity: isButtonDisabled() ? 0.6 : 1,
           }}
-          onClick={(e) => (step === 14 ? handleRoute(e) : handleStep(e))}
+          onClick={(e) => (step === 7 ? handleRoute(e) : handleStep(e))}
         >
-          {step === 14 ? "Voir l'estimation" : "Suivant"}
+          {step === 7 ? "Voir l'estimation" : "Suivant"}
         </button>
         {error ? (
           <p className="text-red-300 text-xs font-light lg:w-3/5 text-start ml-2">
