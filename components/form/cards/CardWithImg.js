e1a949227bc12a044782f04d90e7de0d37726594
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-19 13:05:56 +0200

    Update CardWithImg.js

--- components/form/cards/CardWithImg.js
+++ components/form/cards/CardWithImg.js
@@ -27,23 +27,20 @@ const CardWithImg = (props) => {
   return (
     <div
       onClick={handleClick} // Ajout de l'événement onClick ici
-      className={`relative bg-white px-4 py-2 w-24 lg:w-[115px] border rounded-xl cursor-pointer transition-all duration-100 flex justify-around flex-col items-center hover:shadow-lg ${
+      className={`bg-white px-4 py-3 w-[250px] border rounded-xl cursor-pointer transition-all duration-100 flex justify-between items-center hover:shadow-lg ${
         selected ? "shadow-md" : null
       }`}
       style={{ borderColor: "#3b82f6", marginRight: props.margin }}
     >
-      <div className={`${type === "maison" ? "lg:w-16 w-16" : "lg:w-12 w-10"}`}>
+      {/* <div className={`${type === "maison" ? "lg:w-16 w-16" : "lg:w-12 w-10"}`}>
         <img src={`/${type === "gironde" ? "cardTest.png" : "cardTest.png"}`} />
-      </div>
-      <p
-        className="mt-3 lg:text-normal text-center text-sm"
-        style={{ color: "#3b82f6" }}
-      >
+      </div> */}
+      <p className=" lg:text-normal text-center" style={{ color: "#3b82f6" }}>
         {type === "gironde" ? "Gironde" : "Charente-Maritime"}
       </p>
-      <div className="w-4 h-4 bg-gray-200 rounded-full absolute top-1 right-1">
+      <div className="relative w-5 h-5 bg-gray-200 rounded-full">
         <div
-          className="w-3 h-3 rounded-full absolute top-0.5 right-0.5"
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4 h-4 rounded-full"
           style={{
             backgroundColor: "#3b82f6",
             display: selected ? "" : "none",
