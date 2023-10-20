2c60b2be1cdef2813c2c11b607dc76fd86938ebf
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-20 13:34:38 +0200

    Create index.js

---
+++ pages/estimation/resultat/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Basic from "../../../layout/Basic";
+
+const index = () => {
+  return <Basic height="min-h-[calc(100vh-78px)]"></Basic>;
+};
+
+export default index;
