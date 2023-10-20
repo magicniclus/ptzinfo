351e1f2eabc73313847b90f5ccfc3d30d44f898d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-20 13:35:36 +0200

    Update index.js

--- pages/estimation/resultat/index.js
+++ pages/estimation/resultat/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import Basic from "../../../layout/Basic";
 
 const index = () => {
-  return <Basic height="min-h-[calc(100vh-78px)]"></Basic>;
+  return (
+    <Basic height="min-h-[calc(100vh-78px)]">
+      <h1>Resultat</h1>
+    </Basic>
+  );
 };
 
 export default index;
