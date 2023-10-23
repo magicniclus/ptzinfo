26e598eb282e58c93b7ebd48b9dfac1aaed5e1b4
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-23 10:35:14 +0200

    Update AppartementMaison.js

--- components/form/step/AppartementMaison.js
+++ components/form/step/AppartementMaison.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from "react";
-import CardWithImg from "../cards/CardWithImg";
+import CardsWithBigImg from "../cards/CardsWithBigImg";
+import { useRouter } from "next/router";
 import { useDispatch } from "react-redux";
 
 const AppartementMaison = () => {
   const dispatch = useDispatch();
   const [selectedType, setSelectedType] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (selectedType) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [selectedType]);
 
   useEffect(() => {
     dispatch({
@@ -13,28 +24,44 @@ const AppartementMaison = () => {
     });
   }, [selectedType]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    router.push(`/estimation`);
+  };
+
   return (
-    <>
+    <div className="w-full lg:w-6/12 flex flex-col mb-10 lg:mb-3 ">
       <h2
-        className="text-2xl font-light lg:my-0 my-5"
+        className="text-2xl font-light lg:my-0 my-5 "
         style={{ color: "#3b82f6" }}
       >
         Quel type de bien souhaitez-vous acquerir ?
       </h2>
-      <div className="flex flex-col">
-        {["Maison", "Appartement"].map((type, idx) => (
-          <div className={idx === 1 && "mt-3"}>
-            <CardWithImg
-              key={type}
-              type={type}
-              onSelect={setSelectedType}
-              typeSelected={selectedType}
-              margin="35px"
-            />
-          </div>
-        ))}
-      </div>
-    </>
+      <form className="flex flex-col items-start mt-10" onSubmit={handleSubmit}>
+        <div className="flex">
+          {["Maison", "Appartement"].map((type, idx) => (
+            <div className={idx === 1 && ""}>
+              <CardsWithBigImg
+                key={type}
+                type={type}
+                onSelect={setSelectedType}
+                typeSelected={selectedType}
+                margin="35px"
+              />
+            </div>
+          ))}
+        </div>
+        <button
+          type="submit"
+          disabled={disabled}
+          className={`text-white py-2 px-8 rounded-lg transition ease-in-out duration-100 mt-10 ${
+            !disabled ? "bg-blue-500 hover:shadow-md" : "  bg-blue-500/60"
+          }`}
+        >
+          Estimer
+        </button>
+      </form>
+    </div>
   );
 };
 
