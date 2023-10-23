1dccc9ac3b02dd14fca85811c2293e5e6ed2ee00
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-23 10:41:13 +0200

    Update AppartementMaison.js

--- components/form/step/AppartementMaison.js
+++ components/form/step/AppartementMaison.js
@@ -32,7 +32,7 @@ const AppartementMaison = () => {
   return (
     <div className="w-full lg:w-6/12 flex flex-col mb-10 lg:mb-3 ">
       <h2
-        className="text-2xl font-light lg:my-0 my-5 "
+        className="text-2xl font-light lg:my-0 mt-5"
         style={{ color: "#3b82f6" }}
       >
         Quel type de bien souhaitez-vous acquerir ?
